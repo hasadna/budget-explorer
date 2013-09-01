09a7c092f5b0ce7b69ce1f65c36527e1e95e28e1
Yehonatan Daniv <maggotfish@gmail.com>
2013-09-01 10:59:35 +0300

    Added RequireJS 2.1.8

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 requirejs.config({
-    baseUrl : '/src',
+    // set '/lib' as base URL so all the project's paths needs to start with '../src/'
+    // this will allow simpler integration later with the Open Budgets site
+    baseUrl : '/lib',
     paths   : {
         api         : 'http://dev.openmuni.org.il/static/src/api',
         i18n        : 'http://dev.openmuni.org.il/static/src/i18n',
-        dictionary  : 'dictionary'
+        dictionary  : '../src/dictionary'
     }
 });
 requirejs([
